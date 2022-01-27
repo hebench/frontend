5708921e8223d6db3932b5da1e8d4e71f2a7189d
Documentation <support@hebench.org>
2022-01-26 16:05:24 -0800

    Test Harness: finalized csv parser
    - Added finalized version of csv parser. CSV dataset loading feature is
    working now.

--- dataset_loader/src/hebench_dataset_loader.cpp
+++ dataset_loader/src/hebench_dataset_loader.cpp
@@ -19,58 +19,89 @@ struct icsvstream : std::istringstream
 {
     static constexpr const char *s_trim = " \t\n\r\f\v";
 
-    struct csv_ws : std::ctype<char>
-    {
-        static const mask *make_table()
-        {
-            static std::vector<mask> v(classic_table(), classic_table() + table_size);
-            v[','] |= space;
-            //v[' '] &= ~space;
-            return &v[0];
-        }
-        csv_ws(std::size_t refs = 0) :
-            ctype(make_table(), false, refs) {}
-    };
-    static std::locale loc;
     template <class... Args>
     icsvstream(Args &&... args) :
         std::istringstream(std::forward<Args>(args)...)
     {
         exceptions(std::ifstream::failbit);
-        imbue(loc);
     }
 };
 
-std::locale icsvstream::loc = std::locale(std::locale(), new icsvstream::csv_ws());
+template <typename T>
+icsvstream &operator>>(icsvstream &in, T &arg)
+{
+    std::string s;
+    std::getline(in, s, ',');
+    std::istringstream iss(s);
+    iss.exceptions(std::ifstream::failbit);
+    try
+    {
+        iss >> arg;
+    }
+    catch (const std::ios_base::failure &e)
+    {
+        std::ostringstream oss;
+        int status;
+        oss << e.what() << " in data type \"" << abi::__cxa_demangle(typeid(T).name(), 0, 0, &status) << "\" from \"" << s << "\"";
+        throw std::ios_base::failure(oss.str(), e.code());
+    }
+    return in;
+}
+
+template <>
+icsvstream &operator>><std::string>(icsvstream &in, std::string &arg)
+{
+    char c;
+    while (std::isspace(c = in.get()))
+        ;
+    if (c == '"')
+    {
+        std::getline(in, arg, '"');
+        try
+        {
+            std::string s;
+            std::getline(in, s, ',');
+        }
+        catch (const std::ios_base::failure &ex)
+        {
+            if (!in.eof())
+                throw;
+        }
+    }
+    else
+    {
+        in.unget();
+        std::getline(in, arg, ',');
+        arg.erase(arg.find_last_not_of(" \n\r\t") + 1);
+    }
+    return in;
+}
 
 template <typename T>
-// returns actual number of lines read
-std::size_t loadcsvdatafile(std::ifstream &ifs, std::vector<std::vector<T>> &v, size_t nlines, size_t skip = 0, size_t lnum = 0, std::string fpath = "")
+static void loadcsvdatafile(std::ifstream &ifs, size_t &lnum, std::vector<std::vector<T>> &v, size_t nlines, size_t skip = 0, std::string fpath = "")
 {
-    std::size_t retval = 0;
     std::string line;
     while (skip--)
     {
         ++lnum;
         std::getline(ifs, line);
     }
-    while (nlines-- and std::getline(ifs, line))
+    while (std::getline(ifs, line))
     {
-        ++retval;
         ++lnum;
         line.erase(0, line.find_first_not_of(icsvstream::s_trim));
         line.erase(line.find_last_not_of(icsvstream::s_trim) + 1);
         if (line.size() == 0 or line.at(0) == '#')
-        {
-            ++nlines;
             continue;
-        }
         icsvstream ils(line);
         std::vector<T> w;
-        std::istream_iterator<T> isit(ils);
         try
         {
-            std::copy(isit, std::istream_iterator<T>(), std::back_inserter(w));
+            T x;
+            while (!ils.eof() and ils >> x)
+            {
+                w.push_back(x);
+            }
         }
         catch (const std::ios_base::failure &e)
         {
@@ -79,9 +110,7 @@ std::size_t loadcsvdatafile(std::ifstream &ifs, std::vector<std::vector<T>> &v, 
                 //throw; // preserves error type and attributes
                 std::ostringstream oss;
                 size_t ofs = ils.rdbuf()->pubseekoff(0, std::ios_base::cur, std::ios_base::in);
-                int status;
-                oss << e.what() << " in data type \"" << abi::__cxa_demangle(typeid(T).name(), 0, 0, &status) << "\""
-                    << " at " << fpath << ":" << lnum << ":" << ofs << std::endl;
+                oss << e.what() << " at " << fpath << ":" << lnum << ":" << ofs << std::endl;
                 throw std::ios_base::failure(oss.str(), e.code());
             }
         }
@@ -92,31 +121,13 @@ std::size_t loadcsvdatafile(std::ifstream &ifs, std::vector<std::vector<T>> &v, 
             throw std::length_error(ss.str());
         }
         v.push_back(w);
+        if (nlines and --nlines == 0)
+            break;
     }
-    if ((nlines + 1) != 0)
+    if (nlines)
     {
-        throw(std::length_error("Not enought lines read before end of file."));
+        throw(std::length_error("Not enought lines read before end of file in " + fpath));
     }
-
-    return retval;
-}
-
-template <typename T>
-icsvstream &operator>>(icsvstream &in, T &arg)
-{
-    std::istringstream *iss = &in;
-    try
-    {
-        (*iss) >> arg; // superclass read data into argument
-    }
-    catch (const std::ios_base::failure &e)
-    {
-        std::ostringstream oss;
-        int status;
-        oss << e.what() << " in data type \"" << abi::__cxa_demangle(typeid(T).name(), 0, 0, &status) << "\"";
-        throw std::ios_base::failure(oss.str(), e.code());
-    }
-    return in;
 }
 
 template <typename T, typename E>
@@ -137,7 +148,18 @@ ExternalDataset<T> ExternalDatasetLoader<T, E>::loadFromCSV(const std::string &f
         icsvstream iss(metaline);
         std::string tag, kind;
         size_t ix, nlines;
-        iss >> tag >> ix >> nlines >> kind;
+        try
+        {
+            iss >> tag >> ix >> nlines >> kind;
+        }
+        catch (const std::ios_base::failure &e)
+        {
+            //throw; // preserves error type and attributes
+            std::ostringstream oss;
+            size_t ofs = iss.rdbuf()->pubseekoff(0, std::ios_base::cur, std::ios_base::in);
+            oss << e.what() << " at " << filename << ":" << lnum << ":" << ofs << std::endl;
+            throw std::ios_base::failure(oss.str(), e.code());
+        }
         std::vector<std::vector<std::vector<T>>> *u;
         if (tag == "input")
             u = &eds.inputs;
@@ -145,9 +167,9 @@ ExternalDataset<T> ExternalDatasetLoader<T, E>::loadFromCSV(const std::string &f
             u = &eds.outputs;
         else
         {
-            std::stringstream ss;
-            ss << "Parse error: <tag> must be \"input\" or \"output\". Got: " << tag << " at "
-               << filename << ":" << lnum;
+            std::ostringstream ss;
+            ss << "Parse error at " << filename << ":" << lnum
+               << ": <tag> must be \"input\" or \"output\". Got: " << tag;
             throw std::runtime_error(ss.str());
         }
         if (ix >= u->size())
@@ -155,38 +177,50 @@ ExternalDataset<T> ExternalDatasetLoader<T, E>::loadFromCSV(const std::string &f
         std::vector<std::vector<T>> &v = u->at(ix);
         if (kind == "csv")
         {
-            std::string dataline;
-            while (nlines > 0 && std::getline(ifs, dataline))
+            while (nlines--)
             {
-                ++lnum;
+                std::string dataline;
+                std::getline(ifs, dataline);
                 dataline.erase(0, dataline.find_first_not_of(icsvstream::s_trim));
                 dataline.erase(dataline.find_last_not_of(icsvstream::s_trim) + 1);
-                if (dataline.empty() || dataline.front() == '#')
+                if (dataline.size() == 0 or dataline.at(0) == '#')
+                {
+                    ++nlines;
                     continue;
+                }
                 icsvstream ils(dataline);
                 std::string fname;
                 size_t from_line = 1, num_lines = 0;
-                ils >> fname >> from_line >> num_lines;
+                ils >> fname;
+                try
+                {
+                    ils >> from_line >> num_lines;
+                }
+                catch (const std::ios_base::failure &e)
+                {
+                    if (!ils.eof())
+                        throw;
+                }
                 std::filesystem::path path = fname;
                 if (path.is_relative())
                     path = std::filesystem::path(filename).remove_filename() / path;
                 path = std::filesystem::canonical(path);
                 std::ifstream ifs_csv(path, std::ifstream::in);
                 ifs_csv.exceptions(std::ifstream::badbit);
-                loadcsvdatafile(ifs_csv, v, num_lines, from_line - 1, 0, path);
+                std::size_t tmp_lnum = 0;
+                loadcsvdatafile(ifs_csv, tmp_lnum, v, num_lines, from_line - 1, path);
                 ifs_csv.close();
-                --nlines;
             }
         }
         else if (kind == "local")
         {
-            lnum += loadcsvdatafile(ifs, v, nlines, 0, lnum, filename);
+            loadcsvdatafile(ifs, lnum, v, nlines, 0, filename);
         }
         else
         {
-            std::stringstream ss;
-            ss << "Parse error: <kind> must be \"local\" or \"csv\". Got: " << kind << " at "
-               << filename << ":" << lnum;
+            std::ostringstream ss;
+            ss << "Parse error at " << filename << ":" << lnum
+               << ": <kind> must be \"local\" or \"csv\". Got: " << kind;
             throw std::runtime_error(ss.str());
         }
     }
